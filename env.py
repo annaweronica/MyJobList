7ce08031171e282658c2334925ac63e425bec0f6
annaweronica <anna.janiak@yahoo.pl>
2020-04-11 14:20:32 +0000

    Remove env.py

--- env.py
+++
@@ -1,5 +0,0 @@
-import os
-
-os.environ.setdefault("MONGO_URI", "mongodb+srv://anna13:Aster1381@cluster0-rfwzk.mongodb.net/MyJobList?retryWrites=true&w=majority")
-os.environ.setdefault("PORT", "5000")
-os.environ.setdefault("IP", "0.0.0.0")
